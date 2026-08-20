00942c7b72a141b6ec85b5b73b69199f241a9a85
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2026-08-20 20:46:08 +0200

    Implement configuration validation and integrate DatabaseModule into AppModule

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
     imports: [
-        NestConfigModule.forRoot()
+        NestConfigModule.forRoot({
+            validationSchema: Joi.object({
+                MONGODB_URI: Joi.string().required(),
+            })
+        })
     ],
+    exports: [NestConfigModule],
 })
 export class ConfigModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 
-@Module({})
+@Module({
+	imports: [
+		ConfigModule,
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				uri: configService.get<string>('MONGODB_URI'),
+			}),
+		}),
+	],
+})
 export class DatabaseModule {}
