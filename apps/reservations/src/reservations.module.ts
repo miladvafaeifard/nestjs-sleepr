67773818c346539c73e165dd917bc3d3b289a330
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2026-08-22 18:30:08 +0200

    Create a resource for Reservations module with DTOs, service, and controller

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
-import { DatabaseModule } from '@app/common/database';
+import { ReservationsController } from './reservations.controller';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
