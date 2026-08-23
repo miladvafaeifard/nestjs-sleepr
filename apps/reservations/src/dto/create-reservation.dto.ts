9052083528ba7c68c86b62dadfd645e0e52c7c8b
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2026-08-23 11:42:57 +0200

    Enhance CreateReservationDto with placeId and invoiceId, add validation pipes in main.ts, and create HTTP test file for reservations

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,7 +1,26 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+    @IsDate()
+    @Type(() => Date)
+    @IsNotEmpty()
     startDate!: Date;
+
+    @IsDate()
+    @Type(() => Date)
+    @IsNotEmpty()
     endDate!: Date;
+
+    @IsString()
+    @IsNotEmpty()
     userId!: string;
+
+    @IsString()
+    @IsNotEmpty()
     placeId!: string;
+
+    @IsString()
+    @IsNotEmpty()
     invoiceId!: string;
 }
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { Logger } from 'pino-nestjs';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const logger = app.get(Logger);
   app.useLogger(logger);
   const configService = app.get(ConfigService);
